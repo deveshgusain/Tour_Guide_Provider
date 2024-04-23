806aed1a25603d88ab33da45e591450e198f647c
Devesh Singh Gusain <devesh.singh.gusain@sap.com>
2024-04-23 16:34:42 +0530

    modified versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,54 @@
-const path = require("path");
+// const path = require("path");
+
+// module.exports = {
+//   mode: "development",
+//   entry: path.resolve(__dirname, "src", "app"),
+//   output: {
+//     path: path.resolve(__dirname, "public"),
+//     filename: "bundle.js",
+//     publicPath: "/",
+//   },
+//   resolve: {
+//     extensions: [".js", ".jsx"],
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?/,
+//         loader: "babel-loader",
+//       },
+//     ],
+//   },
+// };
+
+const path = require('path');
 
 module.exports = {
-  mode: "development",
   entry: path.resolve(__dirname, "src", "app"),
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
     publicPath: "/",
   },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        loader: "babel-loader",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
     ],
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
+
